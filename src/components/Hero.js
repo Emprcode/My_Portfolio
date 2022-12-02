8e23bcbdf05dad6b2a786e7f3e9328a484f7ba40
Narayan Aryal <aryal.coding@gmail.com>
2022-12-02 21:06:14 +1100

    modefied

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-import fox from "../assets/fox.jpg";
+import image from "../assets/my_image.jpeg";
 
 export const Hero = () => {
   return (
@@ -9,7 +9,7 @@ export const Hero = () => {
           {/* <!-- .row>.col*2  --> */}
           <div class="row py-5 ">
             <div class="col-md-5 text-center order-md-2 mt-5 ">
-              <img src={fox} alt="my-image" width="80%" />
+              <img src={image} alt="my-image" width="80%" />
             </div>
             <div class="col-md-7 mt-5 pt-5 ">
               Hi <span class="fw-bold">NARAYAN ARYAL</span>
